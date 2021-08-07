3dca9f97daa52e1bfb302683af242409543e8e11
matiasop <movalle4@uc.cl>
2021-08-06 20:39:39 -0400

    add all quotes and quotes styles

--- book-quotes/pages/quotes/[id].tsx
+++ book-quotes/pages/quotes/[id].tsx
@@ -2,6 +2,8 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import data from '../../data/quotes_complete_indexed.json'
 import QuoteType from '../../utils/QuoteType'
 import getQuotesList from '../../utils/getQuotesList'
+import styles from '../../styles/Quote.module.css'
+import Image from 'next/image'
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getQuotesList(data).map((quote: QuoteType) => {
@@ -26,10 +28,23 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 const Quote = ({ quote }: { quote: QuoteType }) => {
   return (
-    <div>
-      <p>Quote Details</p>
-      <h1>{quote.book_title}</h1>
-      <p>{quote.quote}</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>{quote.book_title}</h1>
+      <p className={styles.author}>{quote.author}</p>
+      <div className={styles.flex}>
+        <Image className={styles.image} src={String(quote.thumbnail) || '/book.jpg'} alt="book" width={150} height={200} />
+        <div className={styles.content}>
+          <p className={styles.publishedDate}>Published Date: {quote.published_date}</p>
+          <p className={styles.readingDate}>Reading Date: {quote.date} {quote.time}</p>
+          <p className={styles.pos}>Position: {quote.initial_pos}-{quote.final_pos}</p>
+        </div>
+        <div className={styles.content}>
+          <p className={styles.isbn}>ISBN: {quote.isbn_13 || quote.isbn_10}</p>
+          <p className={styles.lang}>Language: {quote.language}</p>
+          <p className={styles.pages}>Page Count: {quote.page_count.toString()}</p>
+        </div>
+      </div>
+      <p className={styles.quote}>{`"${quote.quote}"`}</p>
     </div>
   )
 }
--- book-quotes/pages/quotes/index.tsx
+++ book-quotes/pages/quotes/index.tsx
@@ -3,6 +3,9 @@ import { GetStaticProps } from "next";
 import data from '../../data/quotes_complete_indexed.json'
 import QuoteType from '../../utils/QuoteType'
 import getQuotesList from '../../utils/getQuotesList'
+import Image from 'next/image'
+import { string } from 'prop-types';
+import styles from '../../styles/Quotes.module.css'
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const quotesList: QuoteType[] = getQuotesList(data);
@@ -16,16 +19,23 @@ const Quotes = ({ quotesList }: { quotesList: QuoteType[] }) => {
   return (
     <div>
       <h1>All Quotes</h1>
-      {quotesList.map((q: QuoteType) => (
-        <div key={q.quote_id.toString()}>
-          <Link href={`/quotes/${q.quote_id.toString()}`}>
-            <a>
-              <h3>{q.book_title}</h3>
-              <p>{q.quote}</p>
-            </a>
-          </Link>
-        </div>
-      ))}
+      <div className={styles.cardContainer}>
+        {quotesList.map((q: QuoteType) => (
+          <div key={q.quote_id.toString()} className={styles.card}>
+            <Link href={`/quotes/${q.quote_id.toString()}`}>
+              <a className={styles.flex}>
+                <Image className={styles.image} src={String(q.thumbnail) || '/book.jpg'} alt="book" width={150} height={200} />
+                <div className={styles.content}>
+                  <h3 className={styles.title}>{q.book_title}</h3>
+                  <p className={styles.author}>{q.author}</p>
+                  <p className={styles.date}>{q.date}</p>
+                  <p className={styles.quote}>{`"${q.quote}"`}</p>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
