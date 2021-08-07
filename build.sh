7dc284f08430f486d519ab03cd041943a411d5da
matiasop <movalle4@uc.cl>
2021-08-07 13:06:51 -0400

    add build file

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# create quotes json file
 cd my-clippings
 # python main.py
 python ids.py
 cd ..
 cp my-clippings/quotes_complete_indexed.json book-quotes/data/
+
+# build nextjs
+cd book-quotes
+npm run build
