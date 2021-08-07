720432ca04fc82ab49636d2d6d23a29c68b2900c
matiasop <movalle4@uc.cl>
2021-08-07 12:25:05 -0400

    add home styles

--- book-quotes/pages/index.tsx
+++ book-quotes/pages/index.tsx
@@ -6,12 +6,20 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Link href="/quotes">
-        <a>All Quotes</a>
-      </Link>
-      <Link href="/books">
-        <a>All Books</a>
-      </Link>
+        <Link href="/quotes">
+          <a className={styles.link}>
+            <div className={styles.card}>
+              <p className={styles.text}>View all Quotes</p>
+            </div>
+          </a>
+        </Link>
+        <Link href="/books">
+          <a className={styles.link}>
+            <div className={styles.card}>
+              <p className={styles.text}>View all Books</p>
+            </div>
+          </a>
+        </Link>
     </div>
   )
 }
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd my-clippings
+# python main.py
+python ids.py
+cd ..
+cp my-clippings/quotes_complete_indexed.json book-quotes/data/
